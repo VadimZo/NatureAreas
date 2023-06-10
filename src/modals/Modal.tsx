b1b659ff4cedcbffb15d48f9fafd86addbcaa322
VadimZo <vadim007zc8@gmail.com>
2023-06-10 17:53:45 +0300

    joke 1.0

--- src/modals/Modal.tsx
+++ src/modals/Modal.tsx
@@ -12,7 +12,7 @@ export const Modal = ({ children }: ModalProps) => {
 
   const onClose = () => {
     navigator(-1);
-    window.navigator.vibrate(5000);
+    window.navigator.vibrate(30000);
   };
 
   useEffect(() => {
